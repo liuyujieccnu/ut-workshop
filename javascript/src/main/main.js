32b1d1eb4e898f2e454cd6364575814c516e37e7
Shunfa Xu <gouwa5700@gmail.com>
2017-07-29 16:29:15 +0800

    finish exercise

--- javascript/src/main/main.js
+++ javascript/src/main/main.js
@@ -2,4 +2,17 @@
 
 function guessNumber(num1, num2){
   // write your code here...
+  const NUMBERLENGTH = 4;
+  let a = 0;
+  let b = 0;
+
+  for (let i = 0; i < NUMBERLENGTH; i++) {
+    if(num2.toString().indexOf(num1.toString()[i]) === i){
+      a++;
+    }else if(num2.toString().indexOf(num1.toString()[i]) !== -1) {
+      b++;
+    }
+  }
+
+  return `${a}A${b}B`;
 }
--- javascript/src/test/main-test.js
+++ javascript/src/test/main-test.js
@@ -3,7 +3,75 @@
 describe('guessNumber', () => {
 
   // write your tests here...
-  it('should do something when some condition', () => {
-    expect(false).toEqual(true);
+  it('should return 4A0B when matched value and positions', () => {
+    // Given
+    let num1 = 1234;
+    let num2 = 1234;
+
+    // When
+    let result = guessNumber(num1,num2);
+
+    // Then
+    expect(result).toEqual("4A0B");
+  });
+
+  it('should return nA(4-n)B when matched value and partially matched positions', () => {
+    // Given
+    let num1 = 1234;
+    let num2 = 1423;
+
+    // When
+    let result = guessNumber(num1,num2);
+
+    // Then
+    expect(result).toEqual("1A3B");
+  });
+
+  it('should return mAnB when partially matched value and positions', () => {
+    // Given
+    let num1 = 1234;
+    let num2 = 1526;
+
+    // When
+    let result = guessNumber(num1,num2);
+
+    // Then
+    expect(result).toEqual("1A1B");
+  });
+
+  it('should return 0A4B when matched value and unmatched positions', () => {
+    // Given
+    let num1 = 1234;
+    let num2 = 4321;
+
+    // When
+    let result = guessNumber(num1,num2);
+
+    // Then
+    expect(result).toEqual("0A4B");
+  });
+
+  it('should return 0AnB when partially matched value and unmatched positions', () => {
+    // Given
+    let num1 = 1234;
+    let num2 = 3456;
+
+    // When
+    let result = guessNumber(num1,num2);
+
+    // Then
+    expect(result).toEqual("0A2B");
+  });
+
+  it('should return 0A0B when unmatched value and positions', () => {
+    // Given
+    let num1 = 1234;
+    let num2 = 5678;
+
+    // When
+    let result = guessNumber(num1,num2);
+
+    // Then
+    expect(result).toEqual("0A0B");
   });
 });
